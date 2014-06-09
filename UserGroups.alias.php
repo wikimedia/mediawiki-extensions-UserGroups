6b50b4c21454d40f63df5a469a22909114ceac87
withoutaname <drevitchi@gmail.com>
2014-06-09 14:22:01 -0700

    Initializing extension files for git repository
    Change-Id: I626d10a5797ebfcb3d1e463e6da46fae417855b0

---
+++ UserGroups.alias.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Aliases for Special:UserGroups
+ *
+ * @file
+ * @ingroup Extensions
+ */
+// @codingStandardsIgnoreFile
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'UserGroups' => array( 'UserGroups' ),
+);
